46156e4e1ead6d93f60c857dc356daa1bae9d5a1
matthewbstanton <matthew@Matthews-MacBook-Pro.local>
2012-10-06 10:21:07 -0400

    Login now directs to welcome page

--- src/Login.php
+++ src/Login.php
@@ -2,47 +2,21 @@
 	include("Classes/UserAccess.php");
 	session_start();
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
 		$useraccess = new UserAccess();
-		$login = $useraccess->login("ADMIN", "password");
+		$login = $useraccess->login($username, $password);
 		
 		if ($login == False) {
 			print("login failed");
 		}
 		else {
-			print("login success");
+			session_regenerate_id();
+			
+			$_SESSION['SESS_MEMBER_ID'] = $username;
+			
+			header("location: Welcome.php");
 		}
-		
-		//UserAccess::setUsername("ADMIN");
-		//$login = UserAccess::login();
-		/*$link = mysql_connect($mysql_hostname, $mysql_user, $mysql_password);
-		if (!$link) {
-			die('Not Connected : ' . mysql_error());
-		}
-		
-		$db_selected = mysql_select_db($mysql_database, $link);
-		if (!$db_selected) {
-		    die ('Can\'t use db : ' . mysql_error());
-		}
-				
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		
-		$sql = "SELECT PASSCODE
-				FROM USERS
-				WHERE USERNAME = '$username'";
-				
-		$result = mysql_query($sql) or die(mysql_error());
-		$row = mysql_fetch_array($result);
-		$passhash = $row['PASSCODE'];
-
-		if($passhash == (hash('sha512', $password))) {
-			print("Good Password");
-		}
-		else {
-			print("Bad Password");
-		}*/
-		
-		//header("location: Welcome.php");
 	}
 ?>
 
